e4f65ddf25a9eb7441bb36c6718816982b24fe39
Kimhye0 <akrnrh3737@naver.com>
2019-06-01 02:37:14 +0900

    회원가입 기능 추가

--- app/src/main/java/com/example/doitvoca/MainActivity.java
+++ app/src/main/java/com/example/doitvoca/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.doitvoca;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+//안드로이드의 화면이 Activity
+//MainActivity가 AppCompatActivity를 상속함
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Signup_button = (Button) findViewById(R.id.Signup_button);
+        Signup_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+// 토스트 메세지 띄우기
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "버튼이 눌렸어요.",Toast.LENGTH_LONG).show();
+    }
+// 버튼 클릭 이벤트
+    public void onButton2Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://alldic.daum.net/"));
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/doitvoca/SignUpActivity.java
@@ -0,0 +1,62 @@
+package com.example.doitvoca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editID;
+    private EditText editPW;
+    private EditText editNickname;
+    private Button Complete_button;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        editID = (EditText) findViewById(R.id.ID);
+        editPW = (EditText) findViewById(R.id.PW);
+        editNickname = (EditText) findViewById(R.id.Nickname);
+        Complete_button = (Button) findViewById(R.id.Complete_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Complete_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String ID = editID.getText().toString().trim();
+                String PW = editPW.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(ID,PW)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else{
+                                    Toast.makeText(SignUpActivity.this,"등록 에러",Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+
+}
